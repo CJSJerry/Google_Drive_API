d0b0a0d08e37e341d700102a2cd35b5b09a187fb
CJSJerry <CJJerryShieh@gmail.com>
2024-07-11 14:32:22 +0200

    Clean up

--- main.py
+++ main.py
@@ -16,24 +16,21 @@ credentials = service_account.Credentials.from_service_account_file(
 service = build('drive', 'v3', credentials=credentials)
 
 def upload_file(file_path, file_name, folder_id):
+    
+    # Set the metadata for the file
     file_metadata = {
         'name': file_name,
         'parents': [folder_id]
     }
+
+    # Upload the file
     media = MediaFileUpload(file_path, resumable=True)
     file = service.files().create(body=file_metadata, media_body=media, fields='id, webViewLink').execute()
-    #print('File ID: %s' % file.get('id'))
     
-    # Set permissions after file creation
-    service.permissions().create(
-        fileId=file['id'],
-        body={'type': 'anyone', 'role': 'reader'}
-    ).execute()
+    # Set permissions
+    service.permissions().create(fileId=file['id'], body={'type': 'anyone', 'role': 'reader'}).execute()
 
-    # Retrieve webViewLink after setting permissions
-    file = service.files().get(fileId=file['id'], fields='webViewLink').execute()
-
-    return file.get('webViewLink')
+    return file['webViewLink']
 
 if __name__ == '__main__':
     link = upload_file('C:\\Users\\CJJer\\coding\\GDrive\\test.txt', 'test.txt', '1kyX7Ald3GBLkxyFX2g_mdC1_6APPvvH_')
